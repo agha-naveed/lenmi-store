0f636b8da45cd857c5aeb241ab5a7392d7b63275
agha-naveed <naveedabs31@gmail.com>
2025-01-09 20:43:13 +0500

    navbar

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router'
+import Home from './Main_Components/Home.jsx'
+import Structure from './Main_Components/Structure.jsx'
+
+
+let route = createBrowserRouter([
+  {
+    path: '/',
+    element: <Structure />,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      }
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={route} />
   </StrictMode>,
 )
