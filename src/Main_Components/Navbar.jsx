4133ba40b1cfbeb7063cfa5839f6f77dbdf19f5e
agha-naveed <naveedabs31@gmail.com>
2025-01-09 21:29:59 +0500

    ...

--- src/Main_Components/Navbar.jsx
+++ src/Main_Components/Navbar.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
+import { FiSearch } from "react-icons/fi";
+import { TiShoppingCart } from "react-icons/ti";
+import { MdOutlineAccountCircle } from "react-icons/md";
+
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className='w-full h-36 bg-slate-800'>
+        <div className='flex'>
+            <div className='logo text-white text-3xl'>Lenmi Store</div>
+            
+            <div className='flex'>
+                <input type="text" className='px-2 rounded-md outline-none' />
+                <FiSearch className='text-2xl text-white bg-slate-800' />
+            </div>
+
+            <div className='text-white flex text-3xl'>
+                <TiShoppingCart />
+                <MdOutlineAccountCircle />
+            </div>
+        </div>
+    </div>
   )
 }
--- src/Main_Components/Structure.jsx
+++ src/Main_Components/Structure.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import "../App.css"
+import Navbar from './Navbar'
+
 
 export default function Structure() {
   return (
     <div>
-        
+        <Navbar />
     </div>
   )
 }
