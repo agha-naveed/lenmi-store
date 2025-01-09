3b90dec9b44ae7a4ec5a398eed34a4b07536eb74
agha-naveed <naveedabs31@gmail.com>
2025-01-09 21:37:48 +0500

    navbar

--- src/Main_Components/Navbar.jsx
+++ src/Main_Components/Navbar.jsx
@@ -6,18 +6,20 @@ import { MdOutlineAccountCircle } from "react-icons/md";
 
 export default function Navbar() {
   return (
-    <div className='w-full h-36 bg-slate-800'>
-        <div className='flex'>
-            <div className='logo text-white text-3xl'>Lenmi Store</div>
-            
-            <div className='flex'>
-                <input type="text" className='px-2 rounded-md outline-none' />
-                <FiSearch className='text-2xl text-white bg-slate-800' />
-            </div>
+    <div className='w-full h-36 bg-slate-800 py-2'>
+        <div className="container mx-auto">
+            <div className='flex w-full justify-between'>
+                <div className='logo text-white text-[25px]'>Lenmi Store</div>
+                
+                <div className='flex w-1/2'>
+                    <input type="text" className='px-2 h-10 w-full rounded-md outline-none' />
+                    <FiSearch className='text-xl p-2 top-[2px] rounded-md w-9 h-9 text-white bg-slate-800 relative -left-9' />
+                </div>
 
-            <div className='text-white flex text-3xl'>
-                <TiShoppingCart />
-                <MdOutlineAccountCircle />
+                <div className='text-white flex text-3xl'>
+                    <TiShoppingCart />
+                    <MdOutlineAccountCircle />
+                </div>
             </div>
         </div>
     </div>
