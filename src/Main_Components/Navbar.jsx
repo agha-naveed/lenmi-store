0392a3575f5c14624c4478c25075d0cb12f958a4
agha-naveed <naveedabs31@gmail.com>
2025-01-10 15:11:20 +0500

    .

--- src/Main_Components/Navbar.jsx
+++ src/Main_Components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router';
 import { FiSearch } from "react-icons/fi";
 import { TiShoppingCart } from "react-icons/ti";
 import { MdOutlineAccountCircle } from "react-icons/md";
@@ -7,7 +8,7 @@ import logo from '../assets/img/logo.webp'
 
 export default function Navbar() {
   return (
-    <div className='w-full h-36 bg-slate-800 py-2'>
+    <div className='w-full h-[116px] bg-slate-800 py-2'>
         <div className="container mx-auto">
             <div className='flex w-full justify-between'>
                 <img decoding='async' className='w-48' src={logo} alt="Lenmi Store Logo" />
@@ -22,6 +23,22 @@ export default function Navbar() {
                     <MdOutlineAccountCircle className='cursor-pointer' title='Account' />
                 </div>
             </div>
+
+
+            <nav className='content-center'>
+                <ul className='text-white flex w-full justify-center p-3 gap-2'>
+                    <li className='flex'>
+                        <Link to={"/"} className='py-2 px-4'>Home</Link>
+                    </li>
+                    <li className='flex'>
+                        <Link to={"/"} className='py-2 px-4'>Products</Link>
+                    </li>
+                    <li className='flex'>
+                        <Link to={"/"} className='py-2 px-4'>Super Deals</Link>
+                    </li>
+                </ul>
+            </nav>
+            
         </div>
     </div>
   )
