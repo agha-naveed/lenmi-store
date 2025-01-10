3e0bfd4ec243b3d67924e8c4f59bddd633c064ed
agha-naveed <naveedabs31@gmail.com>
2025-01-10 21:07:19 +0500

    ...

--- src/Main_Components/Signup.jsx
+++ src/Main_Components/Signup.jsx
@@ -1,19 +1,31 @@
 import React from 'react'
 import logo from '../assets/img/logo.webp'
 import { Link } from 'react-router'
+import { useForm } from 'react-hook-form';
 
 export default function Signup() {
-  return (
-    <div className='flex w-full min-h-screen h-full'>
-        <div className='w-1/2 min-h-full bg-slate-800 grid justify-center content-center'>
-            <img src={logo} className='w-44' alt="Lenmi Store Logo" />
-        </div>
+    const { register, handleSubmit } = useForm();
 
-        <div className='w-full min-h-full grid justify-center content-center'>
-            <form method='post'></form>
-            <p>Already have an account? <Link to="/login">Login</Link></p>
+    return (
+        <div className='flex w-full min-h-screen h-full'>
+            <div className='w-1/2 min-h-full bg-slate-800 grid justify-center content-center'>
+                <img src={logo} className='w-44' alt="Lenmi Store Logo" />
+            </div>
+
+            <div className='w-full min-h-full grid justify-center content-center'>
+                
+                <h2 className='text-4xl font-muli-bold text-center'>Signup</h2>
+                
+                <form method='post'>
+                    <div>
+                        <label htmlFor="">First Name</label>
+                        <input type="text" />
+                    </div>
+                </form>
+
+                <p>Already have an account? <Link to="/login">Login</Link></p>
+            </div>
+            
         </div>
-        
-    </div>
-  )
+    )
 }
