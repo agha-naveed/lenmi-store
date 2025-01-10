50e79f8705d1f94bb85f81992c08f2a0f2b5e457
agha-naveed <naveedabs31@gmail.com>
2025-01-10 11:30:46 +0500

    .

--- src/Main_Components/Navbar.jsx
+++ src/Main_Components/Navbar.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { FiSearch } from "react-icons/fi";
 import { TiShoppingCart } from "react-icons/ti";
 import { MdOutlineAccountCircle } from "react-icons/md";
+import logo from '../assets/img/logo.webp'
 
 
 export default function Navbar() {
@@ -9,16 +10,16 @@ export default function Navbar() {
     <div className='w-full h-36 bg-slate-800 py-2'>
         <div className="container mx-auto">
             <div className='flex w-full justify-between'>
-                <div className='logo text-white text-[25px] font-muli-bold'>Lenmi Store</div>
+                <img decoding='async' className='w-48' src={logo} alt="Lenmi Store Logo" />
                 
                 <div className='flex w-1/2'>
-                    <input type="text" placeholder='What are you looking for?' className='pl-3 pr-[53px] h-10 w-full rounded-md outline-none font-muli-regular' />
-                    <FiSearch title='Search' className='text-xl cursor-pointer p-2 top-[2px] rounded-md w-11 h-9 text-white bg-slate-800 relative -left-11' />
+                    <input type="text" placeholder='What are you looking for?' className='pl-3 pr-[53px] h-[44px] w-full rounded-md outline-none font-muli-regular' />
+                    <FiSearch title='Search' className='text-xl cursor-pointer p-2 top-[2px] rounded-md w-11 h-10 text-white bg-slate-800 relative -left-11' />
                 </div>
 
                 <div className='text-white flex text-[34px] gap-2'>
-                    <TiShoppingCart />
-                    <MdOutlineAccountCircle />
+                    <TiShoppingCart className='cursor-pointer' title='Cart' />
+                    <MdOutlineAccountCircle className='cursor-pointer' title='Account' />
                 </div>
             </div>
         </div>
