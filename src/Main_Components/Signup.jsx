49867d50bfe62b09082aa2c0e01eaab285989145
agha-naveed <naveedabs31@gmail.com>
2025-01-10 22:01:29 +0500

    sign up page done

--- src/Main_Components/Signup.jsx
+++ src/Main_Components/Signup.jsx
@@ -1,10 +1,36 @@
 import React from 'react'
 import logo from '../assets/img/logo.webp'
-import { Link } from 'react-router'
+import { Form, Link } from 'react-router'
 import { useForm } from 'react-hook-form';
 
 export default function Signup() {
-    const { register, handleSubmit } = useForm();
+
+    const { register, handleSubmit, formState: { errors } } = useForm();
+
+
+    const onSubmit = (data) => {
+        // console.log(data)
+        // const res = axios.post("http://localhost:7000/api/create-new-invoice", data)
+        // .then(response => setMessage(response.data)).catch(err => console.warn(err))
+        
+    
+        // if(!message) {
+        //   setMessage(res.data);
+        //   setTimeout(() => {
+        //     navigate('/');
+        //   }, 2000)
+        // }
+        // else {
+        //   setMessage("Error Occured!");
+        // }
+      }
+
+      function restrictSigns(event) {
+        const char = event.key;
+        if (char === '+' || char === '-') {
+            event.preventDefault()
+        }
+      }
 
     return (
         <div className='flex w-full min-h-screen h-full'>
@@ -14,14 +40,31 @@ export default function Signup() {
 
             <div className='w-full min-h-full grid justify-center content-center'>
                 
-                <h2 className='text-4xl font-muli-bold text-center'>Signup</h2>
+                <h2 className='text-4xl p-6 font-muli-bold text-center'>Signup</h2>
                 
-                <form method='post'>
-                    <div>
-                        <label htmlFor="">First Name</label>
-                        <input type="text" />
+                <Form onSubmit={handleSubmit(onSubmit)} className='grid gap-2 font-muli-regular' method='POST'>
+                    <div className='flex gap-2'>
+                        <div className='grid'>
+                            <label htmlFor="">First Name</label>
+                            <input type="text" className='h-9 px-2 rounded-md border border-gray-300' required {...register("first_name")} />
+                        </div>
+                        <div className='grid'>
+                            <label htmlFor="">Last Name</label>
+                            <input type="text" className='h-9 px-2 rounded-md border border-gray-300' required {...register("last_name")} />
+                        </div>
                     </div>
-                </form>
+                    <div className='grid'>
+                        <label htmlFor="">Email</label>
+                        <input type="email" placeholder='e.g: abc@xyz.com' className='h-9 px-2 rounded-md border border-gray-300' required {...register("email")} />
+                    </div>
+                    <div className='grid'>
+                        <label htmlFor="">Phone Number</label>
+                        <input type="number" onKeyDown={(e) => restrictSigns(e)}  className='h-9 px-2 rounded-md border border-gray-300' required {...register("phone_number", {min: 11})} />
+                    </div>
+
+                    <button type='submit' title='Sign up!' className='w-full h-10 transition-all rounded-md border font-muli-semibold bg-slate-800 text-white '>Sign up!</button>
+
+                </Form>
 
                 <p>Already have an account? <Link to="/login">Login</Link></p>
             </div>
