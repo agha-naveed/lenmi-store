067965b8b274751e337d03592a82b427b023c2d8
agha-naveed <naveedabs31@gmail.com>
2025-01-11 18:03:28 +0500

    added packages

--- src/Account.jsx
+++ src/Account.jsx
@@ -5,7 +5,7 @@ import logo from './assets/img/logo.webp'
 export default function Account() {
   return (
     <div className='md:flex grid w-full min-h-screen h-full'>
-      <div className='md:w-1/3 w-full md:min-h-screen h-32 bg-slate-800 grid justify-center content-center'>
+      <div className='md:w-1/2 w-full md:min-h-screen h-32 bg-slate-800 grid justify-center content-center'>
         <img src={logo} className='w-44' alt="Lenmi Store Logo" />
       </div>
       <Outlet />
