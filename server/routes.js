fdc16acac3fc63f66d2fcbe4b3e394ef7694a05c
agha-naveed <naveedabs31@gmail.com>
2025-01-11 15:34:36 +0500

    .

--- server/routes.js
+++ server/routes.js
@@ -2,7 +2,9 @@ const express = require("express")
 const route = express.Router()
 
 route.post('/', (req, res) => {
-    return res.json(res.body)
+    let { first_name, last_name, phone_number, email, password, account_type } = req.body
+    
+    return res.json({msg: "done"})
 })
 
 .get("/get", (req, res) => {
