822a3631281c2099ae04fae62130b8a519566c82
agha-naveed <naveedabs31@gmail.com>
2025-01-11 12:20:36 +0500

    .

--- server/index.js
+++ server/index.js
@@ -1,2 +1,11 @@
 const express = require("express")
+const cors = require("cors")
 const app = express()
+const port = 3000;
+
+app.use(cors())
+app.use(express.urlencoded({ extended: false }))
+
+
+
+app.listen(port, () => console.log(`Server Running on port: ${port}`))
