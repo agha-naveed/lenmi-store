c43aa2648fc0a436b84bffefc26ea198b6a46d34
agha-naveed <naveedabs31@gmail.com>
2025-01-12 15:01:54 +0500

    .

--- src/ExtraComponents/ProductCard.jsx
+++ src/ExtraComponents/ProductCard.jsx
@@ -4,32 +4,36 @@ import { FaStar } from "react-icons/fa";
 
 export default function ProductCard() {
   return (
-    <div className='bg-white border border-gray-400 rounded-lg w-52 h-[310px] p-2 grid gap-3'>
-        <div className='w-[190px] h-[170px] group transition_2 overflow-hidden border relative cursor-pointer'>
-            <img src="https://typeshop.pk/wp-content/uploads/2024/09/749789-product-0-i-638518506623874710_06458751-c09a-4dec-b82f-416beff7e94f.webp" className='w-full h-full rounded-lg object-cover absolute transition_2 opacity-100 group-hover:opacity-0' alt="" />
-            <img src="https://img.drz.lazcdn.com/static/pk/p/7b2a2355148607d8c12d443afbae210b.jpg_720x720q80.jpg_.webp" className='w-full h-full rounded-lg object-cover transition_2  group-hover:opacity-100 opacity-0 absolute' alt="" />
-        </div>
-
-        <div>
-            <h2 className='text-[16.3px] tracking-[-1.2px] leading-5 font-muli-semibold line-clamp-2'><Link> Lenovo | N21 | 6th Gen | 16GB Storage | 4GB RAM | 11.6″ Display | Rotatable Camera | Playstore Supported | Chromebook Daraz Like New Slightly Used Import American Stock</Link></h2>
-
-            <div className='flex gap-2'>
-                <div className='font-muli-bold tracking-[-2px]'>PKR <span className='text-[28px]'>833</span></div>
-                <span className='line-through content-end text-[14px] relative bottom-1 text-gray-500'>PKR 980</span>
+    <div>
+        <div className='bg-white border border-gray-400 group rounded-lg w-52 h-[310px] p-2 grid gap-3'>
+            <div className='w-[190px] h-[170px] group transition_2 rounded-lg overflow-hidden border relative cursor-pointer'>
+                <img src="https://typeshop.pk/wp-content/uploads/2024/09/749789-product-0-i-638518506623874710_06458751-c09a-4dec-b82f-416beff7e94f.webp" className='w-full h-full object-cover absolute transition_2 opacity-100 group-hover:opacity-0' alt="" />
+                <img src="https://img.drz.lazcdn.com/static/pk/p/7b2a2355148607d8c12d443afbae210b.jpg_720x720q80.jpg_.webp" className='w-full h-full object-cover transition_2  group-hover:opacity-100 opacity-0 absolute' alt="" />
             </div>
 
-            <div className='flex text-[13px] my-1 gap-1'>
-                <div className='flex text-slate-900 cursor-pointer'>
-                    <FaStar />
-                    <FaStar />
-                    <FaStar />
-                    <FaStar />
-                    <FaStar />
+            <div>
+                <h2 className='text-[16.3px] tracking-[-1.2px] leading-5 font-muli-semibold line-clamp-2'><Link> Lenovo | N21 | 6th Gen | 16GB Storage | 4GB RAM | 11.6″ Display | Rotatable Camera | Playstore Supported | Chromebook Daraz Like New Slightly Used Import American Stock</Link></h2>
+
+                <div className='flex gap-2'>
+                    <div className='font-muli-bold tracking-[-2px]'>PKR <span className='text-[28px]'>833</span></div>
+                    <span className='line-through content-end text-[14px] relative bottom-1 text-gray-500'>PKR 980</span>
                 </div>
-                <p className='text-gray-600'><span>10000+</span> sold</p>
+
+                <div className='flex text-[13px] my-1 gap-1'>
+                    <div className='flex text-slate-900 cursor-pointer'>
+                        <FaStar />
+                        <FaStar />
+                        <FaStar />
+                        <FaStar />
+                        <FaStar />
+                    </div>
+                    <p className='text-gray-600'><span>10000+</span> sold</p>
+                </div>
+
             </div>
 
         </div>
+        <button className='bg-slate-800 text-white h-9 w-full'>Add to Cart</button>
     </div>
   )
 }
--- src/ExtraComponents/TrendingProducts.jsx
+++ src/ExtraComponents/TrendingProducts.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 export default function TrendingProducts() {
   return (
-    <div className='w-56 h-96 bg-slate-400'>
+    <div className='w-56 h-full border border-black rounded-lg p-2'>
+        <div className='w-full justify-items-center grid gap-2'>
+            <div className='w-[210px] h-[200px] p-2 group transition_2 rounded-lg overflow-hidden border relative cursor-pointer'>
+                <img src="https://img.drz.lazcdn.com/static/pk/p/7b2a2355148607d8c12d443afbae210b.jpg_720x720q80.jpg_.webp" className='w-full h-full object-cover transition_2  ' />
+            </div>
+
+            <div className='flex'>
+                <div className='w-full h-full group transition_2 rounded-lg overflow-hidden border relative cursor-pointer'>
+                    <img src="https://typeshop.pk/wp-content/uploads/2024/09/749789-product-0-i-638518506623874710_06458751-c09a-4dec-b82f-416beff7e94f.webp" className='w-full h-full object-cover transition_2  ' />
+                </div>
+                <div className='w-full h-full group transition_2 rounded-lg overflow-hidden border relative cursor-pointer'>
+                    <img src="https://typeshop.pk/wp-content/uploads/2024/09/749789-product-0-i-638518506623874710_06458751-c09a-4dec-b82f-416beff7e94f.webp" className='w-full h-full object-cover transition_2  ' />
+                </div>
+            </div>
+        </div>
 
     </div>
   )
