c52d23d24cbdd8da85b0c014c4cafcad273c5622
agha-naveed <naveedabs31@gmail.com>
2025-01-12 20:00:17 +0500

    bcrypt data

--- server/controllers/userdata.js
+++ server/controllers/userdata.js
@@ -1,15 +1,21 @@
 const User = require('../config/UserSchema')
+const bcrypt = require("bcrypt")
 
 async function  addUserData(req, res) {
     let { first_name, last_name, phone_number, email, password, account_type } = req.body
-    User.insertMany([{
-        first_name,
-        last_name,
-        phone_number,
-        email,
-        password,
-        account_type
-    }])
+    
+    bcrypt.genSalt(10, function(err, salt) {
+        bcrypt.hash(password, salt, function(err, hash) {
+            User.insertMany([{
+                first_name,
+                last_name,
+                phone_number,
+                email,
+                password: hash,
+                account_type
+            }])
+        });
+    });
     
     return res.json({msg: "done"})
 }
